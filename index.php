3e10212ce89d2cf16d0f7e8d1cb8b2ec16a65298
Guillaume <g.veyat@ntymail.com>
2016-10-10 15:05:12 +0200

    grille finie

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <title>Catalogue</title>
     <meta name="name" content="content">
+    <!-- <link rel="stylesheet" href="css/simple-grid.css" media="screen" title="no title" charset="utf-8"> -->
     <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="css/simple-grid.css" media="screen" title="no title" charset="utf-8">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
 </head>
@@ -26,30 +26,21 @@
     </nav>
 
 <div class="container">
-  <div class="row">
-    <div class="col-2 center">
-      <p>Marque 1</p>
-      <p>Marque 2</p>
-      <p>Marque 3</p>
-      <p>Marque 4</p>
-      <p>Marque 5</p>
+  <!-- <div class="row"> -->
+    <div class="flexbox">
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
     </div>
-    <div class="col-10">
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
+    <div class="flexbox">
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
+      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
     </div>
-    <div class="col-10">
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
-      <div class="col-3"><div class="case"></div></div>
-    </div>
-</div>
+<!-- </div> -->
 </div>
 
-    <footer></footer>
+    <footer><p>footer</p></footer>
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
 </body>
 
