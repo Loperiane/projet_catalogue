5313def1cb22897f4d10d7365013ced79a671a20
loicct <loic.chapon.tardy@gmail.com>
2016-10-10 11:56:28 +0200

    new orga

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Catalogue</title>
     <meta name="name" content="content">
-    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css/simple-grid.min.css" media="screen" title="no title" charset="utf-8">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="banniere">
-  </div>
-  <nav>
-    <ul class="navigation">
-      <li>Accueil</li>
-      <li>Guitare</li>
-      <li>Batterie</li>
-      <li>Basse</li>
-      <li>Piano</li>
-      <li>Home-studio</li>
-    </ul>
-  </nav>
-  <div class="sidebar">
+    </div>
+    <nav>
+        <ul class="navigation">
+            <li>Accueil</li>
+            <li>Guitare</li>
+            <li>Batterie</li>
+            <li>Basse</li>
+            <li>Piano</li>
+            <li>Home-studio</li>
+        </ul>
+    </nav>
+    <div class="sidebar">
 
-  </div>
-  <div class="container">
+    </div>
+    <div class="container">
 
-  </div>
-  <footer></footer>
+    </div>
+    <footer></footer>
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
