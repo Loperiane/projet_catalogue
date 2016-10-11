3a21d64aa9a4f666ee993de7d4adb8f73d3f2cfa
loicct <loic.chapon.tardy@gmail.com>
2016-10-11 11:26:22 +0200

    grille ok

--- index.php
+++ index.php
@@ -48,47 +48,138 @@
         </div>
     </nav>
 
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="sidebar-nav">
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <span class="visible-xs navbar-brand">Menu</span>
+                    </div>
+                    <div class="navbar-collapse collapse sidebar-navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="titre-menu">Guitare</li>
 
-    <div class="container">
-        <!-- <div class="row"> -->
-        <div class="flexbox">
-            <div class="col-4">
-                <div class="case">
-                    <div class="caseBleue"></div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="case">
-                    <div class="caseBleue"></div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="case">
-                    <div class="caseBleue"></div>
+                            <li><a href="#">Fender</a></li>
+                            <li><a href="#">Gibson</a></li>
+                            <li><a href="#">Martin</a></li>
+
+                            <li class="titre-menu">Basse</li>
+
+                            <li><a href="#">Fender</a></li>
+                            <li><a href="#">ESP</a></li>
+                            <li><a href="#">Gretsch</a></li>
+
+                            <li class="titre-menu">Batterie</li>
+
+                            <li><a href="#">Pearl</a></li>
+                            <li><a href="#">Sonor</a></li>
+                            <li><a href="#">Yamaha</a></li>
+
+                            <li class="titre-menu">Piano</li>
+
+                            <li><a href="#">Yamaha</a></li>
+                            <li><a href="#">Seiler</a></li>
+                            <li><a href="#">Steinway</a></li>
+
+                            <li class="titre-menu hs">Home-studio</li>
+                            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="headingOne">
+                                        <h4 class="h-titre panel-title">
+                                            <a class="titre-hs" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                              Interfaces audio
+                                            </a>
+                                        </h4>
+                                    </div>
+                                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                        <div class="panel-body">
+                                            <li><a href="#">Motus</a></li>
+                                            <li><a href="#">Presonus</a></li>
+                                            <li><a href="#">M-Audio</a></li>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="headingTwo">
+                                        <h4 class="panel-title">
+                                            <a class="collapsed titre-hs" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                              Enceintes de monitoring
+                                            </a>
+                                          </h4>
+                                    </div>
+                                    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                        <div class="panel-body">
+                                            <li><a href="#">KRK</a></li>
+                                            <li><a href="#">Yamaha</a></li>
+                                            <li><a href="#">M-Audio</a></li>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="headingThree">
+                                        <h4 class="panel-title">
+                                            <a class="collapsed titre-hs" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                              Microphones
+                                            </a>
+                                          </h4>
+                                    </div>
+                                    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                        <div class="panel-body">
+                                            <li><a href="#">Rode</a></li>
+                                            <li><a href="#">Shure</a></li>
+                                            <li><a href="#">Sennheiser</a></li>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="flexbox">
-            <div class="col-4">
+        <div class="col-sm-10">
+            <div class="flexbox">
                 <div class="case">
                     <div class="caseBleue"></div>
                 </div>
-            </div>
-            <div class="col-4">
                 <div class="case">
                     <div class="caseBleue"></div>
                 </div>
-            </div>
-            <div class="col-4">
                 <div class="case">
                     <div class="caseBleue"></div>
                 </div>
-            </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+<div class="case">
+    <div class="caseBleue"></div>
+</div>            </div>
         </div>
-
-
-
     </div>
+
+
 
     <footer>
         <p>footer</p>
