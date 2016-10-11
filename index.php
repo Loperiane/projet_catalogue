a7883e71113ad7fbb8010aba8b1e5ab522fa04b4
loicct <loic.chapon.tardy@gmail.com>
2016-10-11 08:56:30 +0200

    nav bootstrap

--- index.php
+++ index.php
@@ -6,55 +6,97 @@
     <title>Catalogue</title>
     <meta name="name" content="content">
     <!-- <link rel="stylesheet" href="css/simple-grid.css" media="screen" title="no title" charset="utf-8"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
 </head>
 
 <body>
-    <div class="banniere">
-    </div>
-    <nav>
-        <ul class="navigation">
-            <li>
-                <a class="active" id="acc" href="#">Accueil</a></li>
-            <li>
-                <a class="hamb" id="hamb" href="#">☰</a></li>
-            <li>
-                <a href="#" id="guit">Guitare</a></li>
-            <li>
-                <a href="#" id="batt">Batterie</a></li>
-            <li>
-                <a href="#" id="bass">Basse</a></li>
-            <li>
-                <a href="#" id="pian">Piano</a></li>
-            <li>
-                <a href="#" id="hs">Home-studio</a></li>
-        </ul>
+
+    <div class="banniere"></div>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Accueil</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Guitare</a></li>
+                    <li><a href="#">Basse</a></li>
+                    <li><a href="#">Batterie</a></li>
+                    <li><a href="#">Piano</a></li>
+                    <li><a href="#">Home-studio</a></li>
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control recherche" id="" placeholder="Recherche">
+                    </div>
+                    <button type="submit" class="btn btn-default glyphicon glyphicon-search"></button>
+                </form>
+            </div>
+        </div>
     </nav>
 
-<div class="container">
-  <!-- <div class="row"> -->
-    <div class="flexbox">
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-    </div>
-    <div class="flexbox">
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-      <div class="col-4"><div class="case"><div class="caseBleue"></div></div></div>
-    </div>
-<<<<<<< HEAD
-    <footer></footer>
-    <script src="js/script.js"></script>
-=======
-<!-- </div> -->
-</div>
 
-    <footer><p>footer</p></footer>
->>>>>>> catalogue/master
-    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+    <div class="container">
+        <!-- <div class="row"> -->
+        <div class="flexbox">
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox">
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="case">
+                    <div class="caseBleue"></div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <footer>
+        <p>footer</p>
+    </footer>
+
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
