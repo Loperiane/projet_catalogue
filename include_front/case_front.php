013389c8152dae4560bf6a66cd17242af0fca2f6
loicct <loic.chapon.tardy@gmail.com>
2016-10-12 09:30:04 +0200

    Ajout du hover en js

--- include_front/case_front.php
+++ include_front/case_front.php
@@ -2,14 +2,16 @@
     <?php include 'include_front/pagination.php'; ?>
     <div class="flexbox">
 
-        <div class="case">
+        <div class="case" id="case" onmouseover="caseHover(this)" onmouseout="normalCase(this)">
 
             <a href="#" data-toggle="modal" data-target="#myModal" class="clik">
-                <img class="img-case" src="img/zoom.png" alt="" />
+                <img class="img-case" id="img-case" src="img/zoom.png" alt="" />
+                <p id="resume-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem facere ad quibusdam, obcaecati magni, id qui explicabo sapiente, repudiandae quae maiores. Delectus veniam voluptas, nihil dolorem quae perspiciatis, aspernatur voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, unde ratione illo saepe voluptatibus quidem vel eius, magnam velit animi commodi repellendus aliquam voluptates inventore quo quas sed non.</p>
                 <div class="caseBleue">
                     <div class="t-caseBleue">En savoir<br> plus >></div>
                 </div>
             </a>
+
         </div>
 
         <div class="case">
