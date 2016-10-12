0a5b93bafd151200d73b7e241963e2d9c11bfe15
loicct <loic.chapon.tardy@gmail.com>
2016-10-12 11:54:07 +0200

    petites modif modal

--- include_front/modal_produit.php
+++ include_front/modal_produit.php
@@ -4,16 +4,16 @@
 
         <div class="modal-content">
 
-            <div class="modal-header">
+            <div class="modal-header" id="modal-header">
               <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">Test Produit</h4>
+              <h4 class="modal-title" id="modal-titre">Nom Produit</h4>
             </div>
 
         <div class="modal-body">
 
             <img src="img/zoom.png" class="pull-left img-responsive" id="img-modal">
 
-            <p style="font-size: 2rem;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore odit incidunt iure nulla enim aperiam laboriosam minima tempora expedita tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio incidunt eius, soluta culpa tempore delectus sequi laborum laboriosam molestias, eligendi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis necessitatibus incidunt blanditiis dignissimos ipsum quasi neque et eum vitae magnam excepturi, quae aperiam dolore laudantium similique unde tenetur ullam, harum culpa eius? Aperiam veniam excepturi pariatur neque nam quas, qui ipsam, molestiae aspernatur aut ad dolore, nemo est, natus ratione.</p>
+            <p id="modal-paragraphe">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore odit incidunt iure nulla enim aperiam laboriosam minima tempora expedita tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio incidunt eius, soluta culpa tempore delectus sequi laborum laboriosam molestias, eligendi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis necessitatibus incidunt blanditiis dignissimos ipsum quasi neque et eum vitae magnam excepturi, quae aperiam dolore laudantium similique unde tenetur ullam, harum culpa eius? Aperiam veniam excepturi pariatur neque nam quas, qui ipsam, molestiae aspernatur aut ad dolore, nemo est, natus ratione.</p>
 
 
         <!--div class="modal-footer">-->
